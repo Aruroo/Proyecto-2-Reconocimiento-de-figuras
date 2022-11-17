488359b173dc98268c1b0232d8b739f9d2a01d72
Aruroo <jonathan.arturo.preciado@ciencias.unam.mx>
2022-11-16 18:32:24 -0600

    encuentra centro implementado.

--- src/ProcesadorImagen.cs
+++ src/ProcesadorImagen.cs
@@ -1,8 +1,165 @@
 
-using ReconocimientoFiguras.Pixel;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System;
+
 
 namespace ReconocimientoFiguras
 {
+    public class Pixel
+
+    {
+
+        private int x;
+
+        private int y;
+
+        private Color color;
+        
+        /// <summary>
+        /// Constructor de la clase Pixel
+        /// </summary>
+        /// <param name="x">Coordenada x del pixel</param>
+        /// <param name="y">Coordenada y del pixel</param>
+        /// <param name="color">Color del pixel</param>
+        public Pixel(int x, int y, Color color)
+        {
+
+            this.x = x;
+
+            this.y = y;
+
+            this.color = color;
+
+        }
+
+        public Color getColor(){
+            return color;
+        }
+
+        /// <summary>
+        /// Comparador de igualdad entre dos pixeles
+        /// </summary>
+        /// <param name="obj">Pixel a comparar</param>
+        /// <returns>True si son iguales, false si no lo son</returns>
+
+        public bool mismoColor(Pixel pixel)
+        {
+
+            return this.color == pixel.color;
+
+        }
+
+        public int getX(){
+            return x;
+        }
+
+        public int getY(){
+            return y;
+        }
+    }
+
+    public class Figura
+    {
+
+        private List<Pixel> pixeles;
+
+        /// <summary>
+        /// Constructor de la clase Figura
+        /// </summary>
+        /// <param name="pixeles">Lista de pixeles que conforman la figura</param>
+        ///Suponemos que todos los pixeles de la lista son del mismo color.
+        public Figura(List<Pixel> pixeles)
+        {
+            this.pixeles = pixeles;
+        }
+        /// <summary>
+        /// Obtiene el color de la figura
+        /// </summary>
+        public Color getColor()
+        {
+            return pixeles[0].getColor();
+        }
+
+        ///<summary>
+        ///agrega el pixel dado a esta figura
+        ///</summary>
+        public void agregaPixel(Pixel pixel)
+        {
+            pixeles.Add(pixel);
+        }
+
+        public string toString()
+        {
+            return "Figura de color " + getColor() + " con " + pixeles.Count + " pixeles";
+        }
+
+        ///<summary>
+        ///encuentra el centro de la figura
+        ///</summary>
+        ///<returns> el Pixel del centro de la figura </returns>
+        public Pixel encuentraCentro()
+        {
+            if(pixeles.Count == 0)
+            {
+                return null;
+            } else
+            {
+                Pixel primerPixel = pixeles[0];
+                Pixel ultimoPixel = pixeles[pixeles.Count -1];  
+                Pixel centro =new Pixel((primerPixel.getX() + ultimoPixel.getX())/2, (primerPixel.getY() + ultimoPixel.getY())/2, primerPixel.getColor());
+                //Buscamos el centro en la lista de pixeles
+                foreach(Pixel pixel in pixeles){
+                    if(pixel.getX() == centro.getX() && pixel.getY() == centro.getY()){
+                    return pixel;
+                    }
+                }
+            }
+
+            return centro;
+            
+        }
+
+        ///<summary>
+        ///algoritmo que traza lineas desde el centro 
+        ///de la figura hasta los pixeles que la conforman, obteniendo los
+        ///puntos máximos de la figura, que serán interpretados como los vértices
+        ///</summary>
+        ///<returns> una lista de pixeles que representan los vértices de la figura </returns>
+        public List<Pixel> encuentraVertices()
+        {
+            List<Pixel> vertices = new List<Pixel>();
+            Pixel centro = encuentraCentro();
+            
+            return vertices;
+        }
+    
+        ///<summary>
+        ///algoritmo que determina que figura es de acuerdo a su número de vértices
+        ///</summary>
+        ///<returns> una cadena con el nombre de la figura:
+        ///Triangulo, Cuadrilatero, Circulo u otro
+        ///</returns>
+        public string determinaFigura()
+        {
+            int numeroVertices = encuentraVertices().Count;
+            if(numeroVertices == 3)
+            {
+                return "Triangulo";
+            } else if(numeroVertices == 4)
+            {
+                return "Cuadrilatero";
+            } else if(numeroVertices == 0)
+            {
+                return "Circulo";
+            } else
+            {
+                return "Otro";
+            }
+
+        }
+    }
 
     public class ProcesadorImagen
     {
@@ -24,7 +181,7 @@ namespace ReconocimientoFiguras
         /// </summary>
         /// <param name="imagen">Imagen a procesar</param>
         /// <returns>Una lista con las figuras presentes en la imagen</returns>
-        private List <Figura> RecorreImagen(){
+        public List <Figura> RecorreImagen(){
             //el color del fondo es el primer pixel en la imagen
             Pixel fondo = new Pixel(0,0,imagen.GetPixel(0,0));
             List <Figura> figuras = new List<Figura>();
@@ -34,10 +191,10 @@ namespace ReconocimientoFiguras
             {
                 for (int j = 0; j < imagen.Height; j++)
                 {
-                    //si el pixel no es del color del fondo
+                    //si el pixel no es del color del fondo, lo agregamos a una figura
                     if (!imagen.GetPixel(i,j).Equals(fondo.getColor()))
                     {
-                        anexaPixel(figuras, imagen.GetPixel(i,j));
+                        anexaPixel(figuras, new Pixel(i,j,imagen.GetPixel(i,j)));
                     }
                 }
             }
@@ -54,7 +211,7 @@ namespace ReconocimientoFiguras
         /// <param name="figuras">Lista de figuras</param>
         private void anexaPixel(List<Figura> figuras, Pixel pixel)
         {
-            boolean agregado = false;
+            bool agregado = false;
 
             foreach (Figura figura in figuras)
             {
@@ -72,15 +229,27 @@ namespace ReconocimientoFiguras
                 Figura figura = new Figura(pixeles);
                 figuras.Add(figura);
             }
+        }      
+    }
 
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce la dirección de la imagen");
+            string nombreImagen = Console.ReadLine();
+            Bitmap imagen = new Bitmap(nombreImagen);
+            ProcesadorImagen procesador = new ProcesadorImagen(imagen);
+
+            Console.WriteLine("La imagen tiene {0} pixeles de ancho y {1} pixeles de largo", imagen.Width, imagen.Height);
+            List<Figura> figuras = procesador.RecorreImagen();
+            foreach (Figura figura in figuras)
+            {
+                Console.WriteLine(figura.toString());
+                Pixel centro = figura.encuentraCentro();
+                Console.WriteLine("Centro: " + centro.getX() + ", " + centro.getY());
+            }
         }
-
-    
-        
-        
-
-        
-
     }
 
 } 
